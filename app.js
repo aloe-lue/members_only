0b2a9c35e45f4e7dddf46c0434e100d0928610af
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-02-12 15:11:17 +0800

    feat: configure application that follows the following configures
    feat: use express-session passport and localStrategy for logging user
    feat: use the planned routes in this case we have 6 grouped routes
      log in
      sign up
      secret code
      log out
      homepage
      create message
    
    feat: take advantage of res.locals.currentUser make passing req.user
    as a tip

--- app.js
+++ app.js
@@ -3,42 +3,55 @@ const app = express();
 const path = require("node:path");
 require("dotenv").config();
 
-// const homePageRouter = require("./routes/homepageRouter/homepageRouter");
-const createUserRouter = require("./routes/createUserRouter/createUserRouter");
-const errorRouter = require("./routes/errorRouter/errorRouter");
-const joinTheClubRouter = require("./routes/joinClubRouter/joinClubRouter");
+const homepageRouter = require("./routes/homepageRouter/homepageRouter");
+const signUpRouter = require("./routes/signUpRouter/signUpRouter");
 const logInRouter = require("./routes/logInRouter/logInRouter");
+const createMessageRouter = require("./routes/createMessageRouter/createMessageRouter");
+const logOutRouter = require("./routes/logOutRouter/logOutRouter");
+const secretCodeRouter = require("./routes/secretCodeRouter/secretCodeRouter");
+
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("./config/localStrategy/localStrategy");
 
 // set views
 const viewPath = path.join(__dirname, "views");
 app.set("view engine", "ejs");
 app.set("views", viewPath);
 
-// serve static file
-const publicPath = path.join(__dirname, "public");
-app.use(express.static(publicPath));
-app.use(express.urlencoded({ extended: true }));
+// serve static files
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
-// app.use("/", homePageRouter);
+// get json obj from html inputs
+app.use(express.urlencoded({ extended: false }));
 
-// sign up form
-app.use("/createUser", createUserRouter);
-
-// log in form
-app.use("/logIn", logInRouter);
-
-// clubhouse leadership
-app.use("/joinTheClub", joinTheClubRouter);
-
-// catch all unknown get router
-app.use("*", errorRouter);
-
-// you want to handle error in the end
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  res.status(500).send(err.message);
+// express session
+const expressSession = session({
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.SECRET_HEHE,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 },
 });
+
+app.use(expressSession);
+app.use(passport.session());
+passport.use(localStrategy.strategy);
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+passport.serializeUser(localStrategy.serializeUserFunc);
+passport.deserializeUser(localStrategy.deserializeUserFunc);
+
+app.use("/", homepageRouter);
+app.use("/sign-up", signUpRouter);
+app.use("/log-in", logInRouter);
+app.use("/secret-code", secretCodeRouter);
+app.use("/create-message", createMessageRouter);
+app.use("/log-out", logOutRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
