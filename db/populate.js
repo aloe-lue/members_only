8321852a213fbb5552b59a19197e0d348392b9bd
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-02-13 14:01:10 +0800

    feat: use argv to populate db in node=prod or node=dev
    feat: use bcrypt to provide secret code in secret codes

--- db/populate.js
+++ db/populate.js
@@ -1,5 +1,7 @@
 const { Client } = require("pg");
 require("dotenv").config();
+const bcryptjs = require("bcryptjs");
+const { argv } = require("node:process");
 
 const sql = `
   CREATE TABLE IF NOT EXISTS users (
@@ -19,16 +21,28 @@ const sql = `
     user_id INTEGER
   );
 
-  CREATE TABLE IF NOT EXISTS membership_status (
+  CREATE TABLE IF NOT EXISTS secret_codes (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    member_ship_codes VARCHAR( 255 )
+    secret_code_for VARCHAR( 255 ),
+    secret_code VARCHAR( 255 )
   );
+
+  INSERT INTO secret_codes (secret_code, secret_code_for)
+  VALUES ('${bcryptjs.hashSync(
+    process.env.ADMIN_PASSWORD,
+    10
+  )}', 'admin_password'),
+    ('${bcryptjs.hashSync(
+      process.env.LEADER_PASSWORD,
+      10
+    )}', 'leader_password');
+
 `;
 
 async function main() {
   console.log("...seeding");
   const client = new Client({
-    connectionString: process.env.LOCAL_DATABASE,
+    connectionString: argv[2],
   });
   await client.connect();
   await client.query(sql);
