42450234d760d6adeea17222a5f09b8368054c06
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-02-13 13:50:33 +0800

    feat:  insert of message from messages table
    feat: delete message from messages table
    feat: read messages from meassges table
    feat: get user by id for auth
    feat: get user by username and id for strict find of user

--- db/indexQuery.js
+++ db/indexQuery.js
@@ -17,7 +17,7 @@ const createUser = async ({ firstName, lastName, username, password }) => {
  */
 const findUserByUsername = async ({ username }) => {
   const { rows } = await pool.query(
-    "SELECT id, username, password FROM users WHERE username = $1",
+    "SELECT id, username, password, first_name, last_name FROM users WHERE username = $1",
     [username]
   );
   return rows;
@@ -31,7 +31,7 @@ const findUserByUsername = async ({ username }) => {
 
 const findUserById = async ({ id }) => {
   const { rows } = await pool.query(
-    "SELECT id, username, password FROM users WHERE id = $1",
+    "SELECT id, username, first_name, last_name, membership_status FROM users WHERE id = $1",
     [id]
   );
   return rows;
@@ -50,11 +50,47 @@ const createMessage = async ({ title, message, createdTime, userId }) => {
 };
 /**
  * display all messages that the user has created
+ * you want to show anonymous user if the membership_status of the user == member
  * @returns rows
  */
+
 const readMessages = async () => {
   const { rows } = await pool.query(
-    "SELECT msg.message, msg.title, msg.create_time, msg.user_id FROM messages msg JOIN users usrs ON msg.user_id = usrs.id"
+    "SELECT t2.id AS message_id, t1.first_name, t1.last_name, t1.id, t1.username, t2.message, t2.title, t2.user_id, t2.created_time FROM users t1 JOIN messages t2 ON t1.id = t2.user_id"
+  );
+  return rows;
+};
+/**
+ * update membership status to depending on the role if it was an admin then 1 otherwise 2
+ * @param {*} param0
+ */
+const deleteMessage = async ({ messageId }) => {
+  await pool.query("DELETE FROM messages WHERE id = $1", [messageId]);
+};
+
+const updateUserMembershipStatus = async ({
+  role,
+  id,
+  firstName,
+  lastName,
+  username,
+}) => {
+  await pool.query(
+    "UPDATE users SET membership_status = $1 WHERE id = $2 AND first_name = $3 AND last_name = $4 AND username = $5",
+    [role, id, firstName, lastName, username]
+  );
+};
+
+/**
+ * compare secret code
+ * @param {*} param0
+ * @returns
+ */
+
+const getSecretCode = async ({ memberRole }) => {
+  const { rows } = await pool.query(
+    "SELECT * FROM secret_codes WHERE secret_code_for = $1",
+    [memberRole]
   );
   return rows;
 };
@@ -65,4 +101,7 @@ module.exports = {
   findUserByUsername,
   findUserById,
   readMessages,
+  deleteMessage,
+  updateUserMembershipStatus,
+  getSecretCode,
 };
