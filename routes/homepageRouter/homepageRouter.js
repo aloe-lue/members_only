2362a5cdab5b7fe30379419e478b7190695574dd
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-02-15 09:14:57 +0800

    feat: handle route post create message

--- routes/homepageRouter/homepageRouter.js
+++ routes/homepageRouter/homepageRouter.js
@@ -3,5 +3,10 @@ const homepageRouter = Router();
 const homepageController = require("../../controllers/homepageController/homepageController");
 
 homepageRouter.get("/", homepageController.getHomepage);
+homepageRouter.post(
+  "/delete-message/:messageId",
+  homepageController.postDeleteMessage
+);
+homepageRouter.post("/create-message", homepageController.postCreateMessage);
 
 module.exports = homepageRouter;
